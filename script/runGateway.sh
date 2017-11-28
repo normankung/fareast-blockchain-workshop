8eb7b9563e29c7da2686f60f869e1eca93efb5cd
Zhi Hua Chien <ling5130030@gmail.com>
2017-11-28 17:08:14 +0800

    edit script

--- script/runGateway.sh
+++ script/runGateway.sh
@@ -1,7 +1,7 @@
 echo "//////////////////////////////////"
 echo "Start to run gateway server"
 echo "//////////////////////////////////"
-cd ../../
+cd ../../hyperledger-fabric-gateway
 GATEWAY_PORT=4001 FABRIC_ORGNAME=org1 FABRIC_MODE=dev MONITOR=false node app.js &
 echo "//////////////////////////////////"
 echo "Started Gateway Server"
--- script/runHyperledger.sh
+++ script/runHyperledger.sh
@@ -2,7 +2,6 @@
 echo "//////////////////////////////////"
 echo "Docker Compose"
 echo "//////////////////////////////////"
-cd ../../../
-cd artifacts/composeFile
+cd ../../hyperledger-fabric-gateway/artifacts/composeFile
 docker-compose -f one-org-solo-couch-dev.yaml up -d
 docker-compose -f mongo.yaml up -d
